1e2795a7071286f96b4b69c0d9f5d03b347f2027
Jace Manshadi <j_manshad@sfu.ca>
2019-04-03 21:21:52 -0700

    restructuring the files so that all the files that are for env setup that I actively use go into their own folder

---
+++ login_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export PS1="\$(date +\"%Y-%b-%d %r\") \u:\h\$(pwd)\n $ "
+
+alias ll='ls -l'
+alias la='ls -la'
+alias l='ls -alF'
+alias ..='cd ..'
+alias ...='cd ../..'
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+alias commands="$DIR/commands.sh"
