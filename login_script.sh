d41269044028bb55bb507a18c9b6f2130344d366
Jace Manshadi <jmanshad@protonmail.com>
2021-01-10 20:23:00 -0800

    removing the commands as they are now shortlinked instead

--- login_script.sh
+++ login_script.sh
@@ -7,11 +7,6 @@ alias la='ls -la'
 alias l='ls -alF'
 alias ..='cd ..'
 alias ...='cd ../..'
-HISTTIMEFORMAT='%Y-%m-%d %T '
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
 alias vim="vim -c \"set clipboard=unnamedplus\""
 
-alias commands="$DIR/commands.sh"
-
-alias update_commands="$DIR/update_commands.sh"
+HISTTIMEFORMAT='%Y-%m-%d %T '
