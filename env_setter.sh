ac9b9d5118c41572cd68ba4bea5376255321dc2c
Jace Manshadi <jmanshad@protonmail.com>
2023-09-01 18:28:40 -0700

    general updates

--- env_setter.sh
+++ env_setter.sh
@@ -6,28 +6,23 @@ alias l='ls -alF'
 alias ..='cd ..'
 alias ...='cd ../..'
 
-if [ "$(uname)" == "Darwin" ]; then
-  PROMPT="%D{%Y-%b-%f %L:%M:%S %p} %n@%m%/
-  $ "
+PS1="\$(date +\"%Y-%b-%d %r\") \u@\h:\$(pwd)\n $ "
+HISTTIMEFORMAT='%Y-%m-%d %T '
 
-  alias history="history -E"
-elif [ "$(uname -s)" == "Linux" ]; then
-  export PS1="\$(date +\"%Y-%b-%d %r\") \u:\h\$(pwd)\n $ "
-
+if [ "$(uname -s)" == "Linux" ]; then
   alias vim="vim -c \"set clipboard=unnamedplus\""
 
   alias killVlc='ps -ef | grep "[u]sr/bin/vlc" | awk "{print $2}" | xargs kill -9'
 
   alias mount_cold_files="sudo mount -t nfs the_machine.local:/volume1/4_Regular_Cold_Files /home/jace/1_Home/0_Cryptomator_Encryped_Folders/4_Regular_Cold_Files"
   alias mount_multimedia_files="sudo mount -t nfs the_machine.local:/volume1/5_Multimedias /home/jace/1_Home/0_Cryptomator_Encryped_Folders/5_Multimedias"
-  HISTTIMEFORMAT='%Y-%m-%d %T '
+
+  # https://www.softwaredeveloper.blog/git-credential-storage-libsecret
+  git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
 fi
 
 alias monero="sudo monero-wallet-gui"
 
 export PATH="/opt/platform-tools_r33.0.3:$PATH"
-alias xclip="xclip -selection c"
-# https://www.softwaredeveloper.blog/git-credential-storage-libsecret
 
 export PATH="/opt/commands:$PATH"
-git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
