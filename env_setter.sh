4aabf46b60206c9e54638ac8c061c9c961e636f8
Jace Manshadi <jaymanshad@proton.me>
2024-05-05 01:54:55 -0700

    this was pointless and unnecessary with the gtk-vim package

--- env_setter.sh
+++ env_setter.sh
@@ -10,13 +10,11 @@ PS1="\$(date +\"%Y-%b-%d %r\") \u@\h:\$(pwd)\n $ "
 HISTTIMEFORMAT='%Y-%m-%d %T '
 
 if [ "$(uname -s)" == "Linux" ]; then
-	alias vim="vim -c \"set clipboard=unnamedplus\""
-	
 	alias killVlc='ps -ef | grep "[u]sr/bin/vlc" | awk "{print $2}" | xargs kill -9'
-	
+
 	alias mount_cold_files="sudo mount -t nfs the_machine.local:/volume1/4_Regular_Cold_Files /home/jace/1_Home/0_Cryptomator_Encryped_Folders/4_Regular_Cold_Files"
 	alias mount_multimedia_files="sudo mount -t nfs the_machine.local:/volume1/5_Multimedias /home/jace/1_Home/0_Cryptomator_Encryped_Folders/5_Multimedias"
-	
+
 	# https://www.softwaredeveloper.blog/git-credential-storage-libsecret
 	uname -a | grep "microsoft" > /dev/null
 	wsl=`echo $?`
