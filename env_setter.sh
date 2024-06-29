67e44a5628ffe9831a144a21e6d061871b143f87
Jace Manshadi <jaymanshad@proton.me>
2024-06-29 00:12:52 -0700

    moving the steps to the README in my security repo instead

--- env_setter.sh
+++ env_setter.sh
@@ -14,15 +14,6 @@ if [ "$(uname -s)" == "Linux" ]; then
 
 	alias mount_cold_files="sudo mount -t nfs the_machine.local:/volume1/4_Regular_Cold_Files /home/jace/1_Home/0_Cryptomator_Encryped_Folders/4_Regular_Cold_Files"
 	alias mount_multimedia_files="sudo mount -t nfs the_machine.local:/volume1/5_Multimedias /home/jace/1_Home/0_Cryptomator_Encryped_Folders/5_Multimedias"
-
-	# https://www.softwaredeveloper.blog/git-credential-storage-libsecret
-	uname -a | grep "microsoft" > /dev/null
-	wsl=`echo $?`
-	if [ "${wsl}" == 0 ]; then
-		git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe"
-	else
-		git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
-	fi
 fi
 
 alias monero="sudo monero-wallet-gui"
